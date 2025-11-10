b05a2d55692f82ce6cb0eaf4ea787527cc105da2
wblu214 <wblu.eric@gmail.com>
2025-11-10 23:25:21 +0800

    使用合约方法和自己构造合约两种方式进行交易

--- testContract.js
+++ testContract.js
@@ -15,33 +15,49 @@ console.log(provider)
 console.log('\n')
 
 //使用钱包进行签名
-const signer = new ethers.Wallet(
+const wallet = new ethers.Wallet(
     process.env.RINKEBY_PRIVATE_KEY,
     provider
 );
-console.log('Wallet created successfully.');
-console.log('Wallet address:', signer.address);
+console.log('Wallet linked successfully.');
+console.log('Wallet address:', wallet.address);
 console.log('\n')
 
 
 const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
 const contractAddress = '0xc15e1ef0b9c3972231639127Afc6F72ea4c5b05D';
 //实例化合约
-const contract = new ethers.Contract(contractAddress, abi, signer)
-//与合约交互
-// await contract.store('7');
-// await contract.addPerson('Alice', '42');
-const currentFavoriteNumber = await contract.retrieve();
-console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
+const contract = new ethers.Contract(contractAddress, abi, wallet);
 
-const nonce = await signer.getTransactionCount();
-console.log('Nonce:', nonce);
-const tx = {
-	nonce: nonce,
-	gasPrice: 20000000000,
-	gasLimit: 1000000,
-	to: null,
-	value: 0,
-	data: "0x" + binary,
-	chainId: 1337,
-};
+const flag = false;
+if(flag){
+	//自己构造交易
+	//create contract interface
+	const iface = new ethers.Interface(abi);
+	const data = iface.encodeFunctionData('store', [7]);
+	const tx = {
+		nonce: await wallet.getNonce(),
+		gasPrice: 20000000000,
+		gasLimit: 1000000,
+		to: contractAddress,
+		value: 0,
+		data: data,
+		chainId: 11155111,
+	};
+
+	//sign the transaction
+	// const signedTx = await wallet.signTransaction(tx);
+	//send the transaction
+	const sentTx = await wallet.sendTransaction(tx);
+	console.log('Transaction hash:', sentTx.hash);
+}else{
+	//使用合约方法构造交易
+        const tx = await contract.store(7);
+        console.log('Transaction hash:', tx.hash);
+        await tx.wait();
+        console.log('Transaction confirmed!');
+		const currentFavoriteNumber = await contract.retrieve();
+		console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
+		console.log('Current nonce is', await wallet.getNonce());
+}
+
